bed16847936dcf0668e85a1093bc2fe9d7b5f62c
Antonio Segura <antsegcan@gmail.com>
2019-03-14 19:16:21 +0100

    Modifying URL

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const runLightHouse = require('./src/lighthouse/lighthouse');
-
 const CONFIG = require('./src/lighthouse/lighthouse.conf');
 
+const url = process.argv[2] || 'https://developers.google.com/web/tools/lighthouse/';
 
-runLightHouse('https://www.iberia.com', CONFIG).then(results => {
+runLightHouse(url, CONFIG).then(results => {
     console.log(results);
 });
--- src/lighthouse/lighthouse.js
+++ src/lighthouse/lighthouse.js
@@ -2,15 +2,10 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 module.exports = function (url, opts, config = null) {
-    // TODO: test without showpaintrects
     return chromeLauncher.launch().then(chrome => {
         opts.port = chrome.port;
         return lighthouse(url, opts, config).then(results => {
-            // use results.lhr for the JS-consumeable output
-            // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
-            // use results.report for the HTML/JSON/CSV output as a string
-            // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-            return chrome.kill().then(() => results.lhr)
+            return chrome.kill().then(() => results.lhr);
         });
     });
 }
